59a36ce5d92f968f0e75b1525ddc04644a268dcf
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-20 15:33:15 +0100

    blog page

--- layouts/blog/BlogPost.tsx
+++ layouts/blog/BlogPost.tsx
@@ -5,11 +5,17 @@ const BlogPost:React.FC=()=>{
 
 
   return (
-    <div className='grid sm:grid-cols-3 grid-cols-1'>
+    <div className='grid sm:grid-cols-3 grid-cols-1 mt-24'>
         {
             blogItems.map((blog,index)=>{
-                return <div key={index} className="shadow-[blog-post-shadow]">
-
+                return <div key={index} className="shadow-blog-post-shadow h-[756px]">
+                      <h1 className="text-[#1E232C] font-[600] 
+                      text-[26px] leading-[1.8] capitalize opacity-[1] font-[Poppins]">
+                          {blog.title}
+                      </h1>
+                      <p className="p">
+                          {blog.post}
+                      </p>
                 </div>
             })
         }
