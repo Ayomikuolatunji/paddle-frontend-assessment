3cc719123fe7f6ec9a015f83058c46e07198bdd1
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-20 18:03:59 +0100

    still on blog

--- layouts/blog/Blog.tsx
+++ layouts/blog/Blog.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import BlogPost from './BlogPost'
 import Hero from './Hero'
+import KnowMore from './KnowMore'
 import RecentArticle from './RecentArticle'
 import Search from './Search'
 
 export default function Blog() {
   return (
-   <div className='sm:w-10/12 mx-auto w-full sm:px-0 px-3'>
-       <Hero/>
-       <Search/>
-       <BlogPost/>
-       <RecentArticle/>
-   </div>
+    <>
+       <div className='sm:w-10/12 mx-auto w-full sm:px-0 px-3'>
+          <Hero/>
+          <Search/>
+          <BlogPost/>
+          <RecentArticle/>
+       </div>
+       {/* what to know more about metrics component */}
+       <KnowMore/>
+    </>
   )
 }
---
+++ layouts/blog/KnowMore.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const KnowMore:React.FC= () => {
+
+
+
+
+  return (
+    <div className='bg-gradient-to-b from-[#213F72]  grid sm:grid-cols-2 grid-cols-1 to-[#19073B] h-[463px] mb-20 w-full'>
+        <div className="content">
+            <h1>what to know more about Metrics ?</h1>
+            <p>Learn who we are and what drives us.</p>
+            <button>Contact us</button>
+        </div>
+        <div className="overlay-box">
+
+        </div>
+    </div>
+  )
+}
+
+export default KnowMore
