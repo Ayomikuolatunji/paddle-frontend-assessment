bee75b276618ec5927d48d366eaf208bf1484448
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-20 11:48:35 +0100

    starting with blog route

---
+++ helpers/blogItems.tsx
@@ -0,0 +1,19 @@
+
+
+export const blogItems=[
+    {   image:"",
+        date:new Date(),
+        title:"12 Popup Use Cases To Increase Conversions",
+        post:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum",
+        feature:"FEATURED ARTICLE . AFFILATE"
+    }
+]
+
+export const articleItems=[
+    {
+        date:new Date(),
+        title:"12 Popup Use Cases To Increase Conversions",
+        post:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum",
+        feature:"FEATURED ARTICLE . AFFILATE"
+    }
+]
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+import React from 'react'
+import Blog from '../../layouts/blog/Blog'
+
+export const index:NextPage = () => {
+  return <Blog/>
+}
