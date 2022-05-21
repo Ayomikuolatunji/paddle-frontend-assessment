edca63c82c8019855359e3079c3357a30803b285
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-21 22:47:36 +0100

    successfully fetch github profiles

--- layouts/github/GitHubPage.tsx
+++ layouts/github/GitHubPage.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchProfiles } from '../../redux/githubSlice'
+import { useAppDispatch } from '../../redux/hook'
+
+
 
 const GitHubPage:React.FC = () => {
+     const dis=useAppDispatch()
+
+    useEffect(()=>{
+      dis(fetchProfiles())
+    },[])
+
+    
   return (
     <div>GitHubPage</div>
   )
--- redux/githubSlice.tsx
+++ redux/githubSlice.tsx
@@ -1,30 +1,44 @@
-import { createSlice, PayloadAction,createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios"
 
-export interface CounterState {
+export interface  profiles {
   profiles: []
+  pending:boolean
+  loading:boolean
 }
 
-const initialState: CounterState = {
+const initialState:  profiles = {
   profiles: [],
-  
+  pending:true,
+  loading:true
 }
 
 export const fetchProfiles=createAsyncThunk("profiles/fetchProfiles",async()=>{
-      
+      const response=await axios(`https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc&page=${1}`)
+      return response.data
 })
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const profileSlice = createSlice({
+  name: 'profiles',
   initialState,
   reducers: {
 
   },
-  extraReducers:{
-       
-  }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfiles.pending, (state, action) => {
+
+      })
+      .addCase(fetchProfiles.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.profiles=action.payload
+      })
+      .addCase(fetchProfiles.rejected, (state, action) => {
+        
+      })
+  },
 })
 
 // Action creators are generated for each case reducer function
 
-
-export default counterSlice.reducer
+export default profileSlice.reducer
