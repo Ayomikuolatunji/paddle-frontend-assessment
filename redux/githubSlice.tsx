5d3caa64b290993ad4cf73f226b2a3563602d6ea
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-21 22:04:47 +0100

    redux store created and createAsync thunk will be used to fetch api

--- redux/githubSlice.tsx
+++ redux/githubSlice.tsx
@@ -1,10 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction,createAsyncThunk } from '@reduxjs/toolkit'
 
-export const store = configureStore({
-  reducer: {},
+export interface CounterState {
+  profiles: []
+}
+
+const initialState: CounterState = {
+  profiles: [],
+  
+}
+
+export const fetchProfiles=createAsyncThunk("profiles/fetchProfiles",async()=>{
+      
 })
 
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
 
-export type RootState = ReturnType<typeof store.getState>
+  },
+  extraReducers:{
+       
+  }
+})
 
-export type AppDispatch = typeof store.dispatch
+// Action creators are generated for each case reducer function
+
+
+export default counterSlice.reducer
