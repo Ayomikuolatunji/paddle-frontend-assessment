f5d1fabd1ee81509ec165898e668ffc948c6b170
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-21 13:59:03 +0100

    still on whyUs

--- layouts/about/WhyUs.tsx
+++ layouts/about/WhyUs.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const WhyUs:React.FC = () => {
+
+
+
+
+    useEffect(()=>
+    )
   return (
-    <div className='sm:w-10/12 mx-auto w-full sm:px-0 px-3 h-[627px]'>
+    <div className='sm:w-10/12 mx-auto w-full mt-28 sm:px-0 px-3 h-[627px]'>
         <div className="main-wrapper grid sm:grid-cols-2 grid-cols-1">
 
             <div className="first-grid">
                 <div className="why-intro">
-                    <div className="readmore flex items-center">
+                    <div className="readmore flex items-center relative">
+                           <span className="text absolute top-0 left-0 -mt-5 opacity-[1] sm:-mt-9 text-[#ffffff] text-[18px] font-medium sm:text-[35px]">
+                               01
+                           </span>
                             <span className='bg-[#FFFFFF] h-[2px] w-[87px] block'></span>
-                            <p className='text-[#FFFFFF] ml-3 cursor-pointer'>Why us
+                            <p className='text-[#FFFFFF] ml-3 cursor-pointer text-xl uppercase text-medium'>Why us
                             </p>
                         </div>
                     </div>
 
                     <div className="text-content">
-                        <div className="content-wrapper">
-                            <p>
+                        <div className="content-wrapper md:w-[70%] sm:w-[85%] w-full">
+                            <p className='text-[22px] text-[#FFFFFF] mt-10 '>
                             We pride ourselves in our ability to innovate and create world-class tools that provide reliable and efficient touchpoints between advertisers and affiliates.
                             </p>
                         </div>
