b9bb47817f7f3d6bd85d1298c62e694f10506d30
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-21 20:22:44 +0100

    contact form

--- layouts/contact/Contact.tsx
+++ layouts/contact/Contact.tsx
@@ -3,14 +3,21 @@ import Form from './form/Form'
 
 const Contact:React.FC = () => {
   return (
-    <div className='bg-gradient-to-r from-[#213F72] z-10 to-[#19073B]  w-full h-[100vh] grid sm:grid-cols-2 grid-cols-1'>
-        <div className="left">
+   <div className='relative'>
+      <div className='bg-gradient-to-r from-[#213F72] z-10 to-[#19073B]  w-full h-[100vh] grid sm:grid-cols-2 grid-cols-1'>
+        <div className="left w-full h-[100vh] cover">
 
         </div>
         <div className="right w-full h-[inherent]">
             <Form/>
         </div>
-    </div>
+      </div>
+      <div className="circle-navigation">
+         <div className="circle">
+           
+         </div>
+      </div>
+   </div>
   )
 }
 
