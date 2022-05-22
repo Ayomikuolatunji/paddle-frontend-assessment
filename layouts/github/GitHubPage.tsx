93ea949e94cc27b063831c222f5789531817b712
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-22 08:17:01 +0100

    styling github fetch profile

--- layouts/github/GitHubPage.tsx
+++ layouts/github/GitHubPage.tsx
@@ -5,14 +5,6 @@ import Loader from '../../util/Loader'
 import GithubProfile from './GithubProfile'
 
 
-interface IUserData {
-    id: number; // ID should not be part of the data
-    name: string;
-    stargazers_count:number 
-    
-}
-
-
 const GitHubPage = () => {
     const [pageNum,setPageNum]=useState(1)
      const dispatch=useAppDispatch()
@@ -26,8 +18,15 @@ const GitHubPage = () => {
     // fetch data from redux store 
     useEffect(()=>{
       dispatch(fetchProfiles(pageNum))
-    },[])
+    },[pageNum])
 
+   useEffect(()=>{
+    window.onscroll = function (){
+      if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+          setPageNum((prevNum)=>prevNum+1)
+      }
+    }
+   })
 
   return (
     <div>
@@ -36,8 +35,8 @@ const GitHubPage = () => {
              <Loader/> : 
              <div>
                  {
-                    profiles.map((profile:IUserData)=>{
-                        return  <GithubProfile profile={profile} key={profile.id}/>
+                    profiles.map((profile,index)=>{
+                        return  <GithubProfile profile={profile} key={index}/>
                     })
                  }
              </div>
--- layouts/github/GithubProfile.tsx
+++ layouts/github/GithubProfile.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import { AiFillStar } from 'react-icons/ai'
 import React from 'react'
 
 
@@ -7,6 +9,12 @@ interface IUserData {
         id:number | string
         name:string
         stargazers_count:number 
+        freeCodeCamp:string
+        description:string
+        open_issues_count:string
+        owner:{
+          avatar_url:string
+        }
     }
 }
 
@@ -14,8 +22,28 @@ interface IUserData {
 const GithubProfile = (prop:IUserData) => {
 
   return (
-    <div key={prop.profile.id} className="w-[75%] shadow-md mx-auto">
-         
+    <div key={prop.profile.id} className="w-[50%] my-3 p-2 shadow-md mx-auto flex  items-center">
+         <div className="image w-[270px] h-[150px] relative">
+             <Image 
+             src={prop.profile.owner.avatar_url} 
+             alt={prop.profile.name}  
+             layout="fill"
+             className='rounded-md'
+             />
+         </div>
+         <div className="details ml-3">
+             <h1 className='mb-3'>{prop.profile.name}</h1>
+             <p className='mb-3'>{prop.profile.description}</p>
+             <div className="repo-detail flex">
+               <p className="star p-4 shadow-lg border-2">
+                  {prop.profile.stargazers_count}
+                  <AiFillStar className='text-yellow-400 inline '/>
+               </p>
+               <p className="star">
+                  {prop.profile.open_issues_count}  
+               </p>
+             </div>
+         </div>
     </div>
   )
 }
